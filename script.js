48c08acb8de413f88a4cb84bd0fb83b30f0713e1
Aligatoree <85631225+Aligatoree@users.noreply.github.com>
2021-09-24 18:36:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let calcButtons = [
+    '1','2','3','+',
+    '4','5','6','-',
+    '7','8','9','*',
+    'C','0','=','/'];
+let numbers = document.getElementById('numbers');
+
+calcButtons.forEach(sign => {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.innerHTML = sign;
+    numbers.appendChild(button);
+    console.log(button);
+});
