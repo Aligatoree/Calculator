5e36e40eb11bbe0c02ddc6c560f80939584e1ffd
Aligatoree <85631225+Aligatoree@users.noreply.github.com>
2021-09-25 18:17:59 +0300

    Add calculations

--- script.js
+++ script.js
@@ -1,14 +1,80 @@
-let calcButtons = [
+let signs = [
     '1','2','3','+',
     '4','5','6','-',
     '7','8','9','*',
     'C','0','=','/'];
-let numbers = document.getElementById('numbers');
+let allButtons = document.getElementById('allButtons');
+let input = document.getElementById('enter');
+let firstNumber = 0;
+let operator = '';
+let result = 0;
 
-calcButtons.forEach(sign => {
+signs.forEach(sign => {
     let button = document.createElement('button');
     button.className = 'button';
     button.innerHTML = sign;
-    numbers.appendChild(button);
+    allButtons.appendChild(button);
 });
 
+document.querySelectorAll('button').forEach(function(button) {
+    button.addEventListener('click', onButtonClick);
+});
+function onButtonClick(event) {
+    let num = event.target.innerHTML;
+    switch (event.target.innerHTML) {
+        case 'C':
+            input.value = '';
+            firstNumber = 0;
+            result = 0;
+            break;
+        case '+':
+            firstNumber += +input.value;
+            operator = '+';
+            input.value = '';
+            break;
+        case '-':
+            firstNumber -= +input.value;
+            operator = '-';
+            input.value = '';
+            break;
+        case '*':
+            firstNumber *= +input.value;
+            operator = '*';
+            input.value = '';
+            break;
+        case '/':
+            firstNumber /= +input.value;
+            operator = '/';
+            input.value = '';
+            break;
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+            input.value += Number(num);
+            break;
+        case '=':
+            switch (operator) {
+                case '+':
+                    input.value = +firstNumber + +input.value;
+                    break;
+                case '-':
+                    input.value = +firstNumber - +input.value;
+                    break;
+                case '*':
+                    input.value = +firstNumber * +input.value;
+                    break;
+                case '/':
+                    input.value = +firstNumber / +input.value;
+                    break;
+                case '':
+                    input.value = firstNumber;
+            }
+    }
+};
