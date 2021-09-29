1687209a5546add3c246c5d38cc59ba8bba9e1bc
Aligatoree <85631225+Aligatoree@users.noreply.github.com>
2021-09-29 16:23:55 +0300

    Improved stability of calculations

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let signs = [
     '7','8','9','*',
     'C','0','=','/'];
 let allButtons = document.getElementById('allButtons');
+let info = document.getElementById('info');
 let input = document.getElementById('enter');
-let firstNumber = 0;
+let firstNumber = '';
+let secondNumber = '';
 let operator = '';
-let result = 0;
+let result = '';
 
 signs.forEach(sign => {
     let button = document.createElement('button');
@@ -19,33 +21,54 @@ signs.forEach(sign => {
 document.querySelectorAll('button').forEach(function(button) {
     button.addEventListener('click', onButtonClick);
 });
+
 function onButtonClick(event) {
     let num = event.target.innerHTML;
     switch (event.target.innerHTML) {
         case 'C':
             input.value = '';
-            firstNumber = 0;
-            result = 0;
+            enterNumber = '';
+            secondNumber = '';
+            operator = '';
+            input.placeholder = 0;
+            result = '';
             break;
         case '+':
-            firstNumber += +input.value;
-            operator = '+';
-            input.value = '';
+            if (enterNumber != '') {
+                result = equal(result, enterNumber);
+                enterNumber = +input.value;
+                input.value = '';
+                input.placeholder = result;
+                operator = '+';
+                break;
+            }
             break;
         case '-':
-            firstNumber -= +input.value;
-            operator = '-';
-            input.value = '';
+            if (enterNumber != '') {
+                result = equal(result, enterNumber);
+                enterNumber = +input.value;
+                input.value = '';
+                input.placeholder = result;
+                operator = '-';
+            }
             break;
         case '*':
-            firstNumber *= +input.value;
-            operator = '*';
-            input.value = '';
+            if (enterNumber != '') {
+                result = equal(result, enterNumber);
+                enterNumber = +input.value;
+                input.value = '';
+                input.placeholder = result;
+                operator = '*';
+            }
             break;
         case '/':
-            firstNumber /= +input.value;
-            operator = '/';
-            input.value = '';
+            if (enterNumber !== '' && enterNumber !== 0) {
+                result = equal(result, enterNumber);
+                enterNumber = +input.value;
+                input.value = '';
+                input.placeholder = result;
+                operator = '/';
+            }
             break;
         case '1':
         case '2':
@@ -57,24 +80,45 @@ function onButtonClick(event) {
         case '8':
         case '9':
         case '0':
-            input.value += Number(num);
+            input.value += +num;
+            enterNumber = +input.value;
             break;
         case '=':
-            switch (operator) {
-                case '+':
-                    input.value = +firstNumber + +input.value;
-                    break;
-                case '-':
-                    input.value = +firstNumber - +input.value;
-                    break;
-                case '*':
-                    input.value = +firstNumber * +input.value;
-                    break;
-                case '/':
-                    input.value = +firstNumber / +input.value;
-                    break;
-                case '':
-                    input.value = firstNumber;
+            if (enterNumber != '') {
+                enterNumber = equal(result, enterNumber);
+                input.value = '';
+                input.placeholder = enterNumber;
+                result = '';
+                operator = '';
             }
+
+
     }
+
+
 };
+
+
+
+function equal(first, second) {
+    if (first === '') {
+        return second;
+    }
+    switch (operator) {
+        case '+':
+            return first + second;
+        case '-':
+            return first - second;
+        case '*':
+            return first * second;
+        case '/':
+            if (second == 0) {
+                input.placeholder = 'Error';
+                input.value = '';
+                enterNumber = '';
+                secondNumber = 0;
+                return 0;
+            } else
+                return first / second;
+    }
+}
